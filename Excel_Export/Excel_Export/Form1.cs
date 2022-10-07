5999ec8b00500e35522053059d1e0c7eca996199
Diána Dudok <diana.dudok@stud.uni-corvinus.hu>
2022-10-07 14:08:40 +0200

    Excel export

--- Excel_Export/Excel_Export/Form1.cs
+++ Excel_Export/Excel_Export/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace Excel_Export
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
         }
     }
 }
