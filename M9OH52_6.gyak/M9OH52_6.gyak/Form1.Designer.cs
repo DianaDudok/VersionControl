72283a93eb0c8c2481983d4f160a46f7dfc515f4
Diána Dudok <diana.dudok@stud.uni-corvinus.hu>
2022-11-03 14:49:28 +0100

    Színválasztó gomb beállítása

--- M9OH52_6.gyak/M9OH52_6.gyak/Form1.Designer.cs
+++ M9OH52_6.gyak/M9OH52_6.gyak/Form1.Designer.cs
@@ -94,6 +94,7 @@ namespace M9OH52_6.gyak
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
--- M9OH52_6.gyak/M9OH52_6.gyak/Form1.cs
+++ M9OH52_6.gyak/M9OH52_6.gyak/Form1.cs
@@ -78,5 +78,16 @@ namespace M9OH52_6.gyak
         {
             Factory = new BallFactory();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+        }
     }
 }
