ca02d9959104baf2e27a4ab54ed5604fb36c2b55
Diána Dudok <diana.dudok@stud.uni-corvinus.hu>
2022-11-03 14:49:04 +0100

    Felhasználói felület szépítése

--- M9OH52_6.gyak/M9OH52_6.gyak/Form1.Designer.cs
+++ M9OH52_6.gyak/M9OH52_6.gyak/Form1.Designer.cs
@@ -63,13 +63,13 @@ namespace M9OH52_6.gyak
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.Size = new System.Drawing.Size(91, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Coming Next";
+            this.label1.Text = "Coming Next:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 56);
+            this.button1.Location = new System.Drawing.Point(12, 151);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -79,7 +79,7 @@ namespace M9OH52_6.gyak
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(16, 95);
+            this.button2.Location = new System.Drawing.Point(12, 190);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -89,7 +89,7 @@ namespace M9OH52_6.gyak
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(16, 136);
+            this.button3.Location = new System.Drawing.Point(12, 229);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
