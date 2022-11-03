0459ba0a99fa91bf336f7db8a2ec689f5cf82d6f
Diána Dudok <diana.dudok@stud.uni-corvinus.hu>
2022-11-03 14:45:21 +0100

    BallFactory és CarFactory szerkesztése

--- M9OH52_6.gyak/M9OH52_6.gyak/Entities/CarFactory.cs
+++ M9OH52_6.gyak/M9OH52_6.gyak/Entities/CarFactory.cs
@@ -1,18 +1,17 @@
 ﻿using M9OH52_6.gyak.Abstractions;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace M9OH52_6.gyak.Entities
 {
-    public class BallFactory : IToyFactory
+    class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
