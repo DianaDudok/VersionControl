75ddc0f2f4fc11ca34fecb55540b97dbf777a144
Diána Dudok <diana.dudok@stud.uni-corvinus.hu>
2022-11-03 14:32:44 +0100

    BallFactory osztály létrehozása

---
+++ M9OH52_6.gyak/M9OH52_6.gyak/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M9OH52_6.gyak.Entities
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
