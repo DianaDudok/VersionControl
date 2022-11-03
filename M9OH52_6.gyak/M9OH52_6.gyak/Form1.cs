43d559cd72c0d70a3f4f2082fb31ecd6fe59f3e8
Diána Dudok <diana.dudok@stud.uni-corvinus.hu>
2022-11-03 14:33:37 +0100

    Entities Mappa behivítkozása, Ball lista létrehozása

--- M9OH52_6.gyak/M9OH52_6.gyak/Form1.cs
+++ M9OH52_6.gyak/M9OH52_6.gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using M9OH52_6.gyak.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,14 @@ namespace M9OH52_6.gyak
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
         public Form1()
         {
             InitializeComponent();
