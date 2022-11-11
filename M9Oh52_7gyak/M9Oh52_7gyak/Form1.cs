9ccc207931ea7577a1bc9d371d3128b8e6f96ee3
Diána Dudok <diana.dudok@stud.uni-corvinus.hu>
2022-11-11 14:12:44 +0100

    Listák létrehozása

---
+++ M9Oh52_7gyak/M9Oh52_7gyak/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using M9Oh52_7gyak.Entities;
+
+namespace M9Oh52_7gyak
+{
+    public partial class Form1 : Form
+    {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+    }
+}
